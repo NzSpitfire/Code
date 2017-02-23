476f4422a907098c2e536b1a22837bc2539223f0
NzSpitfire <brad-101@windowslive.com>
2017-02-23 23:10:10 +1300

    splash

---
+++ SydneyFC/app/src/main/java/au/com/foxsports/sydneyfc/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package au.com.foxsports.sydneyfc.activity;
+
+/**
+ * Created by bclark on 23/02/17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //give one second delay to load data
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+
+    }
+}
